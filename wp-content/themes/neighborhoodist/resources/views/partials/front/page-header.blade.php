1df1086a22db68938011fc4dad9497b97861438b
varunm <varunm@ghrix.com>
2022-02-24 18:32:00 +0530

    24-02-2022 css changes in respective files

--- wp-content/themes/neighborhoodist/resources/views/partials/front/page-header.blade.php
+++ wp-content/themes/neighborhoodist/resources/views/partials/front/page-header.blade.php
@@ -1,23 +1,13 @@
 <div class="page-header page-header--front">
+<div class="bg-container">
+    @if (get_post_thumbnail_id())
+      {!! App::acfimg(get_post_thumbnail_id(),'large') !!}
+      @endif
+  </div>
   <div class="container">
-  <div class="stagger-visualizer"></div>
-
-  <div class="header-eyebrow">{{$header_eyebrow}}</div>
-    <h1>{!! App::title() !!}</h1>
+    <h1><img src="/wp-content/themes/neighborhoodist/resources/assets/images/favicon@3x.png" alt="Neighborhoodist Home Icon">Neighborhoodist</h1>
     <div class="header-content">{{$header_content}}</div>
-    <div class="header--front-visualizer-toggles">
-      <ul class="nav" id="myTab" role="tablist">
-        <li class="nav-item" role="presentation">
-          <a class="nav-link active" id="current-tab" data-toggle="tab" href="#current" role="tab" aria-controls="current" aria-selected="true">Current Approach</a>
-        </li>
-        <li class="nav-item" role="presentation">
-          <a class="nav-link" id="solution-tab" data-toggle="tab" href="#solution" role="tab" aria-controls="solution" aria-selected="false">Our Solution</a>
-        </li>
-      </ul>
-      <a href="/get-started" class="btn btn-light">Get Started</a>
-    </div>
-
-    <div class="stagger-visualizer"></div>
+    <a href="#feature-content">Choose Your Neighborhood.</a>
   </div>
 </div>
 
