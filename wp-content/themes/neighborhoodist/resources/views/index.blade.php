ecfbe4732366a9fecd948b3e2d1344a7fc06ad62
Evan Bassler <etbassler@gmail.com>
2022-02-25 16:35:49 -0600

    acf updates and archive fixes

--- wp-content/themes/neighborhoodist/resources/views/index.blade.php
+++ wp-content/themes/neighborhoodist/resources/views/index.blade.php
@@ -4,10 +4,6 @@
   @include('partials.page-header-archive')
 
   <div class="page-content directory-results">
-    <div class="container">
-      <div class="row">
         @php echo do_shortcode('[facetwp template="blog_posts"]') @endphp
-      </div>
-    </div>
 </div>
 @endsection
--- wp-content/themes/neighborhoodist/resources/views/partials/page-header-archive.blade.php
+++ wp-content/themes/neighborhoodist/resources/views/partials/page-header-archive.blade.php
@@ -6,7 +6,7 @@
   </div>
   <div class="container">
     <div class="row">
-      <div class="col-md-8 text-center">
+      <div class="col-md-12 text-center">
         <h1 class="sr-only">{!! App::title() !!}</h1>
         <div class="description-text">Directory Of</div>
         <div class="archive-filters d-flex justify-content-around">
